0845e84efef3e6ab4ee5430d3b72890f09fd312e
이동욱 <jojoldu@gmail.com>
2019-06-07 23:16:58 +0900

    index 컨트롤러 + view 완성

---
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
