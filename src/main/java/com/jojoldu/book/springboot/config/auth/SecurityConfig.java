c4ae91ba69f0c30dcc74d36184d88fcd1bff0e8c
이동욱 <jojoldu@gmail.com>
2019-06-10 23:06:33 +0900

    시큐리티 로그인 성공 여부 어케 보여줄까

--- src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
@@ -5,9 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
-import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.web.filter.CharacterEncodingFilter;
+
+import static org.springframework.http.HttpMethod.POST;
 
 @Configuration
 @EnableWebSecurity
@@ -19,17 +19,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers("/", "/login/oauth2/**", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
-                .antMatchers("/api/v1/**").hasAnyRole(Role.USER.name())
+                .antMatchers(POST, "/api/v1/**").hasAnyRole(Role.USER.name())
                 .anyRequest().authenticated()
                 .and()
                 .oauth2Login()
-                .defaultSuccessUrl("/")
-                .failureUrl("/")
+                .loginPage("/")
                 .and()
                 .headers().frameOptions().disable()
                 .and()
                 .exceptionHandling()
-                .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/"))
                 .and()
                 .formLogin()
                 .successForwardUrl("/")
@@ -37,10 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout()
                 .logoutUrl("/logout")
                 .logoutSuccessUrl("/")
-                .deleteCookies("JSESSIONID")
-                .invalidateHttpSession(true)
                 .and()
-                .addFilterBefore(filter, CsrfFilter.class)
                 .csrf().disable();
     }
 }
