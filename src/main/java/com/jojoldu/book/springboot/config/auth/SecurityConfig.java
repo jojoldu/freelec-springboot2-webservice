06128c59a666dfb8a5569ce9e0ac2c39d77ebba9
이동욱 <jojoldu@gmail.com>
2019-06-19 09:00:15 +0900

    불필요한 코드 삭제

--- src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
@@ -2,18 +2,15 @@ package com.jojoldu.book.springboot.config.auth;
 
 import com.jojoldu.book.springboot.domain.user.Role;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CustomOidcUserService customOidcUserService;
-    private final CustomUserDetailsService customUserDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -23,17 +20,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
-                .antMatchers("/api/v1/**").hasRole(Role.MEMBER.name())
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                 .anyRequest().authenticated()
                 .and()
                 .oauth2Login()
                 .userInfoEndpoint()
                 .oidcUserService(customOidcUserService);
-    }
-
-    @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        return customUserDetailsService;
     }
 }
--- src/main/java/com/jojoldu/book/springboot/domain/user/Role.java
+++ src/main/java/com/jojoldu/book/springboot/domain/user/Role.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 public enum Role {
 
     GUEST("ROLE_GUEST", "손님"),
-    MEMBER("ROLE_MEMBER", "일반 사용자");
+    USER("ROLE_USER", "일반 사용자");
 
     private final String key;
     private final String title;
