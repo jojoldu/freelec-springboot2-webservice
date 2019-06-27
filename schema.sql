526c2abefb60bd135460e2cbf92454e78d1d9264
이동욱 <jojoldu@gmail.com>
2019-06-27 20:07:16 +0900

    스키마 추가

---
+++ schema.sql
@@ -0,0 +1,2 @@
+create table posts (id bigint not null auto_increment, created_date datetime, modified_date datetime, author varchar(255), content TEXT not null, title varchar(500) not null, primary key (id)) engine=InnoDB;
+create table user (id bigint not null auto_increment, created_date datetime, modified_date datetime, email varchar(255) not null, name varchar(255) not null, picture varchar(255), role varchar(255) not null, primary key (id)) engine=InnoDB;
