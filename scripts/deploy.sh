b94ccc058914feb2fc239983d3afa1ec81e8ab49
이동욱 <jojoldu@gmail.com>
2019-07-06 10:33:43 +0900

    한번더 정리

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -27,15 +27,11 @@ JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
-echo "> 실행 포커스를 step2로 이동"
-
-cd $REPOSITORY
-
 echo "> Jar에 실행권한 추가"
 
-chmod +x $JAR_NAME
+chmod +x $REPOSITORY/$JAR_NAME
 
 nohup java -jar \
         -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
         -Dspring.profiles.active=real \
-        $JAR_NAME > /dev/null 2> /dev/null < /dev/null &
+        $REPOSITORY/$JAR_NAME > /dev/null 2> /dev/null < /dev/null &
