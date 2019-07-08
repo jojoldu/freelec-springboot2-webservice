0078f2a79df4807a7f21dea2795f955c8e0de7e0
이동욱 <jojoldu@gmail.com>
2019-07-08 11:18:54 +0900

    코드 리팩토링

--- src/main/java/com/jojoldu/book/springboot/web/ProfileController.java
+++ src/main/java/com/jojoldu/book/springboot/web/ProfileController.java
@@ -17,19 +17,12 @@ public class ProfileController {
     @GetMapping("/profile")
     public String profile() {
         List<String> profiles = Arrays.asList(env.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real", "real1", "real2");
+        String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
 
-        if(profiles.contains("real1")){
-            return "real1";
-        }
-
-        if(profiles.contains("real2")){
-            return "real2";
-        }
-
-        if(profiles.isEmpty()) {
-            return "default";
-        }
-
-        return profiles.get(0);
+        return profiles.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
     }
 }
