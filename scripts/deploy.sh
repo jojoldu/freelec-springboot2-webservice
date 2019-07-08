a7a604962df4f7163393b9f9b3b8c99eaca21667
이동욱 <jojoldu@gmail.com>
2019-07-08 21:48:28 +0900

    마지막 리팩토링

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -34,6 +34,10 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.config.location=\
+        classpath:/application.properties,\
+        classpath:/application-real.properties,\
+        /home/ec2-user/app/application-oauth.properties,\
+        /home/ec2-user/app/application-real-db.properties \
     -Dspring.profiles.active=real \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
