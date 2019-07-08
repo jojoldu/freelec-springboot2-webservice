c86cf0a1957d412e6348c55332aa83da2624b1c5
이동욱 <jojoldu@gmail.com>
2019-07-08 22:05:07 +0900

    오타 수정

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -2,23 +2,23 @@
 
 # bash는 return value가 안되니 *제일 마지막줄에 echo로 해서 결과 출력*후, 클라이언트에서 값을 사용한다
 
-# 쉬고 있는 profile 찾기: set1이 사용중이면 set2가 쉬고 있고, 반대면 set1이 쉬고 있음
+# 쉬고 있는 profile 찾기: real1이 사용중이면 real2가 쉬고 있고, 반대면 real1이 쉬고 있음
 function find_idle_profile()
 {
     RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
     if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
     then
-        CURRENT_PROFILE=set2
+        CURRENT_PROFILE=real2
     else
         CURRENT_PROFILE=$(curl -s http://localhost/profile)
     fi
 
-    if [ ${CURRENT_PROFILE} == set1 ]
+    if [ ${CURRENT_PROFILE} == real1 ]
     then
-      IDLE_PROFILE=set2
+      IDLE_PROFILE=real2
     else
-      IDLE_PROFILE=set1
+      IDLE_PROFILE=real1
     fi
 
     echo "${IDLE_PROFILE}"
@@ -29,7 +29,7 @@ function find_idle_port()
 {
     IDLE_PROFILE=$(find_idle_profile)
 
-    if [ ${IDLE_PROFILE} == set1 ]
+    if [ ${IDLE_PROFILE} == real1 ]
     then
       echo "8081"
     else
