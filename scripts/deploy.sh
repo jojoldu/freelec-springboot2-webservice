1f90777843d400358e29c415b8bcb1a491beee97
이동욱 <jojoldu@gmail.com>
2019-07-08 21:39:26 +0900

    표준 입출력 nohup 포워딩

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,4 +33,4 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup bash -c "java -jar -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties -Dspring.profiles.active=real $JAR_NAME 2>&1 &"
+nohup bash -c "java -jar -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties -Dspring.profiles.active=real $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &"
