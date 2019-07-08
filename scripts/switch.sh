e19ceaf7e8786afea7acf5fde1febc17792bc3a0
이동욱 <jojoldu@gmail.com>
2019-07-09 08:28:25 +0900

    real2 배포

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -11,9 +11,6 @@ function switch_proxy() {
     echo "> Port 전환"
     echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
-    CURRENT_PROFILE=$(curl -s http://localhost/profile)
-    echo "> 엔진엑스 Current Profile : $CURRENT_PROFILE"
-
     echo "> 엔진엑스 Reload"
     sudo service nginx reload
 }
