7e6038edf4ccdce719f475959e99cbfc77256bf7
이동욱 <jojoldu@gmail.com>
2019-07-08 21:33:30 +0900

    오타 수정

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# bash는 return value가 안되니 *제일 마지막줄에 echo로 해서 결과 출력*후, 클라이언트에서 값을 사용한다
+
 # 쉬고 있는 profile 찾기: set1이 사용중이면 set2가 쉬고 있고, 반대면 set1이 쉬고 있음
 function find_idle_profile()
 {
