f34f90b3c5fdd88bda9baf8a419ffe6a3e175a11
donggu1105 <kbh11051@gmail.com>
2021-01-26 16:47:17 +0900

    add annotation

--- src/main/java/com/jojoldu/book/springboot/config/auth/LoginUser.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/LoginUser.java
@@ -4,7 +4,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-//login annotation
+//login annotation 
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface LoginUser {
