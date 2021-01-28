93d907fb3c62d0ed429ce7cde08feecbe6c2ded1
donggu1105 <kbh11051@gmail.com>
2021-01-28 17:46:11 +0900

    test

--- src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -25,7 +25,7 @@ public class PostsRepositoryTest {
     }
 
     @Test
-    public void 게시글저장_불러오기() {
+    public void 게시글저장_불러오기2() {
         //given
         String title = "테스트 게시글";
         String content = "테스트 본문";
