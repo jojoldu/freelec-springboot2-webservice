1262ac18579dc5a9018a6f910c6de73e00ee8b07
smilejakdu <ash982416@gmail.com>
2022-09-28 23:58:31 +0900

    repository

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -2,9 +2,11 @@ package com.jojoldu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository()
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
     @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
--- src/main/java/com/jojoldu/book/springboot/domain/user/UserRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/user/UserRepository.java
@@ -1,9 +1,11 @@
 package com.jojoldu.book.springboot.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository()
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
